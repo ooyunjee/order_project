b3e3519232a7ae9a78ba2193004a459f2b7ca6df
heoyunjee <heoyunjee@gmail.com>
2017-01-12 13:22:10 +0900

    Refactor App

--- order_project/src/components/App.js
+++ order_project/src/components/App.js
@@ -3,19 +3,22 @@ import { BrowserRouter as Router, Link, Match } from 'react-router';
 import Home from './Home';
 import Menu from './Menu';
 
+import '../css/App.css';
+
 class App extends React.Component {
 
   render() {
     return (
       <Router>
         <div>
-          <ul>
-            <li><Link to='/'>Home</Link></li>
-            <li><Link to='/menu'>Menu</Link></li>
-          </ul>
-          <hr/>
-            <Match exactly pattern='/' component={Home}/>
-            <Match pattern='/menu' component={Menu}/>
+          <nav>
+            <ul className="container">
+              <li><Link to='/'>Home</Link></li>
+              <li><Link to='/menu'>Menu</Link></li>
+            </ul>
+          </nav>
+          <Match exactly pattern='/' component={Home}/>
+          <Match pattern='/menu' component={Menu}/>
         </div>
       </Router>
     );
