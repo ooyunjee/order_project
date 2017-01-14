27e6317b3843e20db59c583a0ac851330a602f48
heoyunjee <heoyunjee@gmail.com>
2017-01-15 02:12:27 +0900

    Refactor Home.js
    Change to stateless function

--- order_project/src/components/Home.js
+++ order_project/src/components/Home.js
@@ -3,15 +3,14 @@ import { Link } from 'react-router';
 
 import '../css/Home.css';
 
-export default class Home extends React.Component {
-
-  render() {
-    return (
-        <main className="container home">
-          <h1 className="home-title">plating</h1>
-          <p className="home-desc">셰프의 요리를 집에서</p>
-          <h3 className="home-go-to-menu"><Link to="/menu">주문 하러 가기!</Link></h3>
-        </main>
-    );
-  }
+const Home = () => {
+  return (
+      <main className="container home">
+        <h1 className="home-title">plating</h1>
+        <p className="home-desc">셰프의 요리를 집에서</p>
+        <h3 className="home-go-to-menu"><Link to="/menu">주문 하러 가기!</Link></h3>
+      </main>
+  );
 }
+
+export default Home;
